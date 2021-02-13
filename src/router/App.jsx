51e67f7c7f8e5e5f1cd858de8aa18e8ba06f649b
Juan Prieto <gadaf.78@hotmail.com>
2021-02-12 19:10:26 -0500

    feat: Delete unnecessary files and add first components

---
+++ src/router/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from "../components/Header";
+import { Home } from "../pages/Home";
+
+export const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  );
+};
