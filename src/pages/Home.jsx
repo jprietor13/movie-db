e81f94a31fc3e71a244b77437056d49dac95e818
Juan Prieto <gadaf.78@hotmail.com>
2021-02-13 09:18:15 -0500

    feat: Build home view

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { useGetListMovies } from "../hooks/useGetListMovies";
+import { URL_IMAGE } from "../services/config";
 
 export const Home = () => {
   const { data } = useGetListMovies();
   return (
-    <div>
-      <h4>Home</h4>
+    <div className="home-container">
+      <h4 className="home-container__title">Home</h4>
+      {data.map((item) => (
+        <div className="home-container__item" key={item.id} idmovie={item.id}>
+          <img src={URL_IMAGE + item.poster_path} alt={item.title} />
+          <span className="home-container__item-title">{item.title}</span>
+          <span className="home-container__item-date">{item.release_date}</span>
+        </div>
+      ))}
       {console.log(data)}
     </div>
   );
--- src/router/App.jsx
+++ src/router/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Home } from "../pages/Home";
 
 export const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <Route exact path="/" component={Header} />
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
   );
 };
